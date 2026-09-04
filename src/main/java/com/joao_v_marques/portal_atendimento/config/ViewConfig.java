c739b78b566e53d2f1c5ba93841b597f757a2005
joao-v-marques <joaovmarques2006@outlook.com>
2026-09-04 13:21:15 -0300

    feat: adicionado nova página de login na aplicação (Falta montar a página)

--- src/main/java/com/joao_v_marques/portal_atendimento/config/ViewConfig.java
+++ src/main/java/com/joao_v_marques/portal_atendimento/config/ViewConfig.java
@@ -10,5 +10,6 @@ public class ViewConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
     }
 }
