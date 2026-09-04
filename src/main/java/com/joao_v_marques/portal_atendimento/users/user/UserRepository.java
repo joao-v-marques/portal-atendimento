15ce45537e81068c93cbc10d399fb44fc040082c
joao-v-marques <joaovmarques2006@outlook.com>
2026-09-04 09:24:45 -0300

    feat: adicionado nova função de existsByUsernameIgnoreCase no repository

--- src/main/java/com/joao_v_marques/portal_atendimento/users/user/UserRepository.java
+++ src/main/java/com/joao_v_marques/portal_atendimento/users/user/UserRepository.java
@@ -3,4 +3,6 @@ package com.joao_v_marques.portal_atendimento.users.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    boolean existsByUsernameIgnoreCase(String username);
 }
