fce25fecf3eb527f2972199b5a9f903375d612d5
joao-v-marques <joaovmarques2006@outlook.com>
2026-09-10 10:12:19 -0300

    fix: colocado isActive por padrão como true na entidade, que é o padrão da aplicaçaõ

--- src/main/java/com/joao_v_marques/portal_atendimento/authorization_requests/authorization_status/AuthorizationStatus.java
+++ src/main/java/com/joao_v_marques/portal_atendimento/authorization_requests/authorization_status/AuthorizationStatus.java
@@ -20,5 +20,5 @@ public class AuthorizationStatus {
     private String name;
 
     @Column(name = "is_active", nullable = false)
-    private boolean isActive;
+    private boolean isActive = true;
 }
