e3ca15463df7d83ecb7ac83a1fe816987b1896a2
joao-v-marques <joaovmarques2006@outlook.com>
2026-09-10 15:52:47 -0300

    feat: adicionado nova lógica de POST para AuthorizationRequest

--- src/main/java/com/joao_v_marques/portal_atendimento/authorization/authorization_request/AuthorizationRequestController.java
+++ src/main/java/com/joao_v_marques/portal_atendimento/authorization/authorization_request/AuthorizationRequestController.java
@@ -1,10 +1,16 @@
 package com.joao_v_marques.portal_atendimento.authorization.authorization_request;
 
+import com.joao_v_marques.portal_atendimento.authorization.authorization_request.dto.AuthorizationRequestRequest;
 import com.joao_v_marques.portal_atendimento.authorization.authorization_request.dto.AuthorizationRequestResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.joao_v_marques.portal_atendimento.security.UserPrincipal;
+import jakarta.validation.Valid;
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,5 +26,16 @@ public class AuthorizationRequestController {
     @GetMapping
     public List<AuthorizationRequestResponse> findAll() {
         return authorizationRequestService.findAll();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AuthorizationRequestResponse> create(@Valid @RequestBody AuthorizationRequestRequest request, @AuthenticationPrincipal UserPrincipal currentUser) {
+        Integer currentUserId = currentUser.getId();
+
+        AuthorizationRequestResponse created = authorizationRequestService.create(request, currentUserId);
+
+        URI location = URI.create("/api/authorization-requests/" + created.id());
+
+        return ResponseEntity.created(location).body(created);
     }
 }
--- src/main/java/com/joao_v_marques/portal_atendimento/authorization/authorization_request/AuthorizationRequestService.java
+++ src/main/java/com/joao_v_marques/portal_atendimento/authorization/authorization_request/AuthorizationRequestService.java
@@ -1,18 +1,32 @@
 package com.joao_v_marques.portal_atendimento.authorization.authorization_request;
 
+import com.joao_v_marques.portal_atendimento.authorization.authorization_request.dto.AuthorizationRequestRequest;
 import com.joao_v_marques.portal_atendimento.authorization.authorization_request.dto.AuthorizationRequestResponse;
+import com.joao_v_marques.portal_atendimento.authorization.authorization_status.AuthorizationStatus;
+import com.joao_v_marques.portal_atendimento.authorization.authorization_status.AuthorizationStatusRepository;
+import com.joao_v_marques.portal_atendimento.authorization.authorization_type.AuthorizationType;
+import com.joao_v_marques.portal_atendimento.authorization.authorization_type.AuthorizationTypeRepository;
+import com.joao_v_marques.portal_atendimento.users.user.User;
+import com.joao_v_marques.portal_atendimento.users.user.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class AuthorizationRequestService {
 
     private final AuthorizationRequestRepository authorizationRequestRepository;
+    private final AuthorizationTypeRepository authorizationTypeRepository;
+    private final AuthorizationStatusRepository authorizationStatusRepository;
+    private final UserRepository userRepository;
 
-    public AuthorizationRequestService(AuthorizationRequestRepository authorizationRequestRepository) {
+    public AuthorizationRequestService(AuthorizationRequestRepository authorizationRequestRepository, AuthorizationTypeRepository authorizationTypeRepository, AuthorizationStatusRepository authorizationStatusRepository, UserRepository userRepository) {
         this.authorizationRequestRepository = authorizationRequestRepository;
+        this.authorizationTypeRepository = authorizationTypeRepository;
+        this.authorizationStatusRepository = authorizationStatusRepository;
+        this.userRepository = userRepository;
     }
 
     // GET de todas as autorizações cadastradas
@@ -22,6 +36,43 @@ public class AuthorizationRequestService {
                 .stream()
                 .map(this::toResponse)
                 .toList();
+    }
+
+    @Transactional
+    public AuthorizationRequestResponse create(AuthorizationRequestRequest request, Integer currentUserId) {
+        // Valida as FK's e retorna erro se não existir
+        AuthorizationType authorizationType = authorizationTypeRepository.findById(request.authorizationTypeId())
+                .orElseThrow(() -> new IllegalArgumentException("Não foi encontrado tipo de autorização com o ID informado."));
+        AuthorizationStatus authorizationStatus = authorizationStatusRepository.findById(request.authorizationStatusId())
+                .orElseThrow(() -> new IllegalArgumentException("Não foi encontrado tipo de autorização com o ID informado."));
+        User user = userRepository.findById(currentUserId)
+                .orElseThrow(() -> new IllegalArgumentException("Não foi encontrado usuário com o ID informado."));
+
+        if (!authorizationType.isActive()) {
+            throw new IllegalArgumentException("Não é possível realizar o cadastro de uma autorização com o tipo inativo.");
+        }
+        if (!authorizationStatus.isActive()) {
+            throw new IllegalArgumentException("Não é possível realizar o cadastro de uma autorização com o status inativo.");
+        }
+
+        String transactionNumber = request.transactionNumber().trim();
+        LocalDate requestDate = request.requestDate();
+        String beneficiaryName = request.beneficiaryName().trim();
+        String beneficiaryPhone = request.beneficiaryPhone().trim();
+
+        // Montar a entidade, o DTO nunca vira entidade sozinho
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest();
+        authorizationRequest.setTransactionNumber(transactionNumber);
+        authorizationRequest.setRequestDate(requestDate);
+        authorizationRequest.setAuthorizationType(authorizationType);
+        authorizationRequest.setAuthorizationStatus(authorizationStatus);
+        authorizationRequest.setBeneficiaryName(beneficiaryName);
+        authorizationRequest.setBeneficiaryPhone(beneficiaryPhone);
+        authorizationRequest.setInsertedBy(user);
+
+        AuthorizationRequest saved = authorizationRequestRepository.save(authorizationRequest);
+
+        return toResponse(saved);
     }
 
     private AuthorizationRequestResponse toResponse(AuthorizationRequest authorizationRequest) {
