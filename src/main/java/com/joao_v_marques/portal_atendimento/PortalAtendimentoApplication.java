f597cada523b285ef7578c84c2b9aca88c49141e
joao-v-marques <joaovmarques2006@outlook.com>
2026-09-11 14:27:07 -0300

    feat: adicionado notação para ConfigurationPropertiesScan na inicialização da API

--- src/main/java/com/joao_v_marques/portal_atendimento/PortalAtendimentoApplication.java
+++ src/main/java/com/joao_v_marques/portal_atendimento/PortalAtendimentoApplication.java
@@ -2,8 +2,10 @@ package com.joao_v_marques.portal_atendimento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class PortalAtendimentoApplication {
 
 	public static void main(String[] args) {
