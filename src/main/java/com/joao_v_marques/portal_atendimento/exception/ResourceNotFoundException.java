29ac15896ca9c70263ce8bf60dc6c38fbc068b6e
joao-v-marques <joaovmarques2006@outlook.com>
2026-09-16 09:45:55 -0300

    feat: criado nova exception de notFound para a API

---
+++ src/main/java/com/joao_v_marques/portal_atendimento/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.joao_v_marques.portal_atendimento.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
